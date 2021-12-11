2087162bb17dbbd2ccd1ac802ed6a38d6c35715c
Mirza Kujundzic <mirzak@hotmail.com>
2021-12-11 08:36:07 +0100

    Added login component

--- modules/frontend/src/app/app.module.ts
+++ modules/frontend/src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ modules/frontend/src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  model: any = {};
+  sessionId: any = "";
+
+  constructor(
+      private router: Router,
+      private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    let url = '/api/login';
+    this.http.post<any>(url, {
+      userName: this.model.username,
+      password: this.model.password
+  }).subscribe(res => {
+      if (res) {
+        this.sessionId = res.sessionId;
+          
+        sessionStorage.setItem(
+          'token',
+          this.sessionId
+        );
+        this.router.navigate(['']);
+      } else {
+          alert("Authentication failed.")
+      }
+  });
+}
+
+}
